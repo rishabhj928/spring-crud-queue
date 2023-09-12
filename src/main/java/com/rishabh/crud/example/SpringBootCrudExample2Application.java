b1e823f7177fe2afef1920d95bd61c4ead34a09f
Rishabh <rishabh.jain@myglamm.com>
2023-09-12 16:27:09 +0530

    enable kafka

--- src/main/java/com/rishabh/crud/example/SpringBootCrudExample2Application.java
+++ src/main/java/com/rishabh/crud/example/SpringBootCrudExample2Application.java
@@ -3,8 +3,11 @@ package com.rishabh.crud.example;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @EnableRabbit
+@EnableKafka
 @SpringBootApplication
 public class SpringBootCrudExample2Application {
 
